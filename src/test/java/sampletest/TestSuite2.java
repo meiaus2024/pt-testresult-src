882fd994650a0def5c0e2ad75f9ad2659517ec31
meiaus2024 <165844429+meiaus2024@users.noreply.github.com>
2024-10-11 15:19:38 -0500

    Update TestSuite2.java

--- src/test/java/sampletest/TestSuite2.java
+++ src/test/java/sampletest/TestSuite2.java
@@ -6,12 +6,33 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class TestSuite2 {
 
+   //Uncomment to get the result with 'fail' status, 
+   //but be aware the build will not pass.
+   //Once the test result is validated, comment back to get a smooth build
    //@Test
    public void negativeTest1() {
 
         fail("negativeTest1 to test Failure result");
    }
 
+   //Uncomment to get the result with 'error' status, 
+   //but be aware the build will not pass.
+   //Once the test result is validated, comment back to get a smooth build
+   //@Test
+   public void negativeTest2() throws Exception {
+
+        throw new Exception("negativeTest2 to test Error result");
+
+    }
+
+   @Disabled("negativeTest3 to test Skip result")
+   @Test
+   public void negativeTest3() {
+       //not executed
+     
+   }
+
+}
    //@Test
    public void negativeTest2() throws Exception {
 
