208ea575fe5d96e93311152441603fb0dd231650
meiaus2024 <165844429+meiaus2024@users.noreply.github.com>
2024-10-18 21:10:16 -0500

    Update TestSuite2.java

--- src/test/java/sampletest/TestSuite2.java
+++ src/test/java/sampletest/TestSuite2.java
@@ -33,18 +33,3 @@ public class TestSuite2 {
    }
 
 }
-   //@Test
-   public void negativeTest2() throws Exception {
-
-        throw new Exception("negativeTest2 to test Error result");
-
-    }
-
-   @Disabled("negativeTest3 to test Skip result")
-   @Test
-   public void negativeTest3() {
-       //not executed
-     
-   }
-
-}
